2543dfb666c527ec3acf41c4196293294c636ebb
ankur112358 <ankur112358@gmail.com>
2019-04-23 22:08:34 -0700

    adding player class

---
+++ behaviors/player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zoro on 4/23/19.
+//
+
+#include "player.h"
---
+++ behaviors/player.h
@@ -0,0 +1,14 @@
+//
+// Created by zoro on 4/23/19.
+//
+
+#ifndef UTAUSTINVILLABASE_PLAYER_H
+#define UTAUSTINVILLABASE_PLAYER_H
+
+
+class player {
+
+};
+
+
+#endif //UTAUSTINVILLABASE_PLAYER_H
